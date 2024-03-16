76d5bc1bd62c7f692c106368ce862a0f045b47f7
Syed Mujtaba Hassan <mujhtabahassan1@gmail.com>
2024-03-16 10:07:23 +0500

    Initial Commit

---
+++ AlopeciaAreaMeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlopeciaAreaMeter
+//
+//  Created by Syed Mujtaba Hassan on 16/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
